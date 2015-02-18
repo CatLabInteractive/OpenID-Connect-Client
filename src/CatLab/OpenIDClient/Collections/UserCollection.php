b47e1b2d408418d5a2cbfa0e2540b3488ee1fbdd
Thijs Van der Schaeghe <thijs@catlab.be>
2015-02-18 21:04:37 +0100

    Dum dum dum. Adding logout method.

---
+++ src/CatLab/OpenIDClient/Collections/UserCollection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daedeloth
+ * Date: 5/01/15
+ * Time: 16:09
+ */
+
+namespace CatLab\OpenIDClient\Collections;
+
+
+use Neuron\Collections\Collection;
+
+class UserCollection
+	extends Collection {
+
+
+
+}
